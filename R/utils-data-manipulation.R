314819b8ea056c96428343af7cfc42d3beb589bc
rjake <rjake@sas.upenn.edu>
2020-09-10 21:48:59 -0400

    Add get_article()

--- R/utils-data-manipulation.R
+++ R/utils-data-manipulation.R
@@ -51,3 +51,38 @@ aggregate_group <- function(df, name, ..., calc, cond) {
     as.list()
 }
 
+
+#' Choose "a" or "an"
+#' @param x a number or string
+#' @noRd
+#' @importFrom dplyr case_when
+#' @examples
+#' get_article("increase")
+#' get_article("decrease")
+#' get_article(5)
+#' get_article(8)
+get_article <- function(x) {
+  if (is.character(x)) {
+    ifelse(grepl("^[aeiou]", tolower(x)), "an", "a")
+  } else {
+    x_new <- case_when(
+      x >= 1e6 ~ x / 1e6,
+      x >= 1e3 ~ x / 1e3,
+      TRUE ~ x
+    )
+    x_int <- as.integer(x_new)
+    x_char <- as.character(x_int)
+    n_char <- nchar(x_char)
+
+    case_when(
+      # 80 = an
+      grepl("^8", x_char) ~ "an",
+      # # 11, 11234 = an
+      (grepl("^11", x_char) & n_char %% 2 == 0) ~ "an",
+      # 1, 10, 100 = a
+      (grepl("^1", x_char) & nchar(x_char) <= 3) ~ "a",
+      # else
+      TRUE ~ "a"
+    )
+  }
+}
