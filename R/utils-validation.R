7392f53d2070e6c921bf2f3badc2b81366859667
rjake <rjake@sas.upenn.edu>
2020-09-14 15:20:56 -0400

    Use is.numeric & add roxygen

--- R/utils-validation.R
+++ R/utils-validation.R
@@ -1,7 +1,13 @@
+#' Check that x is vector of length 2
+#' @param x vector
+#' @noRd
+#' @examples
+#' check_valid_vector(letters)
+#' check_valid_vector(1:2)
 check_valid_vector <- function(x) {
   msg <- NULL
 
-  if (!"numeric" %in% is(x)) {
+  if (!is.numeric(x)) {
     msg <- "\nexpecting a numeric vector"
   }
 
