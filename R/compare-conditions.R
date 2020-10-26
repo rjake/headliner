8042de9c16b575ae0eebbc4cfb86598a58d10059
rjake <rjake@sas.upenn.edu>
2020-10-25 20:48:18 -0400

    Split compare-groups into compare-columns and compare-groups

--- R/compare-conditions.R
+++ R/compare-conditions.R
@@ -40,34 +40,3 @@ compare_conditions <- function(df,
   final <- append(res_1, res_2)
   final[order(names(final))]
 }
-
-
-#' Compare two columns within a data frame
-#'
-#' @param df data frame
-#' @param cols columns to compare, supports select helpers like 'starts_with()'
-#' or 'where(is.numeric)'
-#' @param calc named list of the functions to use, ex:
-#' list(mean = mean, sd = sd) 'purrr' style phrases are also supported like
-#' list(mean = ~mean(.x, na.rm = TRUE), sd = sd)
-#' @importFrom dplyr everything
-#' @importFrom glue glue
-#' @export
-#'
-#' @examples
-#' compare_columns(flights_jfk, )
-#' compare_columns(flights_jfk, ends_with("delay"))
-compare_columns <- function(df,
-                            cols = everything(),
-                            calc = list(mean = mean)) {
-  if (nrow(df) == 1) {
-    warning(
-      glue("There is only 1 row in the data frame. Did you mean to use \\
-           compare_values() or headline()?"),
-      call. = FALSE
-    )
-  }
-  res <- aggregate_group(df, "", {{cols}}, calc = calc)
-  res[order(names(res))]
-}
-
