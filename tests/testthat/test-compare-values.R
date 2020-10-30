7804c7f8232a3cff3c3c386a70516e290fb9acb3
rjake <rjake@sas.upenn.edu>
2020-10-29 23:36:54 -0400

    Replace rescale param with multiplier

--- tests/testthat/test-compare-values.R
+++ tests/testthat/test-compare-values.R
@@ -28,11 +28,18 @@ test_that("compare_values produces list", {
 })
 
 
-test_that("rescale works", {
-  whole_numbers <- keep_numeric(compare_values(20, 25))
-  rescaled <- keep_numeric(compare_values(0.2, 0.25, rescale = 100))
-  decimal <- keep_numeric(compare_values(0.2, 0.25, n_decimal = 2))
+test_that("multiplier works", {
+  whole_numbers <- keep_numeric(compare_values(20, 30))
+  multiplierd <- keep_numeric(compare_values(0.2, 0.3, multiplier = 100))
+  decimal <- keep_numeric(compare_values(0.2, 0.3, n_decimal = 2))
 
-  expect_equal(whole_numbers[1:2], rescaled[1:2])
-  expect_equal(rescaled[1] / 100, decimal[1])
+  expect_equal(whole_numbers[1:2], multiplierd[1:2])
+  expect_equal(multiplierd[1] / 100, decimal[1])
+})
+
+test_that("check rounding runs", {
+  expect_warning(compare_values(0.123, 0.12))
+  expect_warning(compare_values(21.1, 21.1))
+  # no wawning
+  expect_warning(compare_values(0.123, 0.1234, n_decimal = 4), regexp = NA)
 })
