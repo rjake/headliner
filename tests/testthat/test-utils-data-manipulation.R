a0d4d19d71bdbcd84a339d73c66636064e7bc2d9
rjake <rjake@sas.upenn.edu>
2020-11-21 20:43:22 -0500

    Add aggregate_group() test

--- tests/testthat/test-utils-data-manipulation.R
+++ tests/testthat/test-utils-data-manipulation.R
@@ -1,3 +1,18 @@
+test_that("aggregate_group() works", {
+  x <-
+    aggregate_group(
+      df = mtcars,
+      name = "_comp",
+      .col = c(mpg, hp),
+      calc = list(avg = mean)
+    )
+
+  expect_equal(names(x), c("avg_mpg_comp", "avg_hp_comp"))
+  expect_equal(x$avg_mpg_comp, mean(mtcars$mpg))
+  expect_equal(x$avg_hp_comp, mean(mtcars$hp))
+})
+
+
 test_that("check rounding throws warnings", {
   expect_warning(check_rounding(0.2, 0.24, n_decimal = 1))
   expect_null(check_rounding(0.2, 0.24, n_decimal = 2))
