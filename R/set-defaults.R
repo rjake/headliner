c02db362acd2bd982474f4e67229f4c093a214e2
rjake <rjake@sas.upenn.edu>
2021-02-14 13:46:20 -0500

    Add function for user to update default values

--- R/set-defaults.R
+++ R/set-defaults.R
@@ -37,6 +37,49 @@ show_headliner_defaults <- function() {
 }
 
 
+#' Set the default values used in making headlines
+#' @param default string using component names from [compare_values()].
+#' The default is \code{"{trend} of {delta} ({orig_values})"}
+#' @export
+#' @seealso [augment_article_patterns()], [show_headliner_defaults()]
+#' @examples
+#' # the way headliner writes out-of-the-box
+#' headline(3, 4)
+#'
+#' # the values headliner uses under the hood
+#' show_headliner_defaults()
+#'
+#' # you can use compare_values(2, 3) %>% view_list() to see all talking
+#' # points but we can use this as a short cut to see some of them:
+#' names(compare_values(2, 3)) %>% head()
+#'
+#' # update headline
+#' set_headliner_defaults(
+#'   headline = "{article_delta_p}% {trend} ({orig_values})",
+#'   orig_values = "from {r} to {c}"
+#' )
+#'
+#' # view changes
+#' show_headliner_defaults()
+#'
+#' # and in action
+#' headline(3, 4)
+#'
+#' # reset values
+#' set_headliner_defaults(headline = NULL, orig_values = NULL)
+#' headline(3, 4)
+set_headliner_defaults <- function(headline = NULL,
+                                   orig_values = NULL) {
+  if (!missing(headline)) {
+    update_default(x = "headline", value = headline)
+  }
+
+  if (!missing(orig_values)) {
+    update_default(x = "orig_values", value = orig_values)
+  }
+}
+
+
 #' Update the values headliner uses in the active session
 #' @noRd
 update_default <- function(x, value) {
