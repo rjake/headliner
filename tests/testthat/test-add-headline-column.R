098ea7978a2e291c38f92e2d929c9df0c5611983
rjake <rjake@sas.upenn.edu>
2021-02-15 22:41:42 -0500

    Add test re: return_cols

--- tests/testthat/test-add-headline-column.R
+++ tests/testthat/test-add-headline-column.R
@@ -9,3 +9,16 @@ test_that("add headline column works", {
     add_headline_column(mtcars, gear, carb, .name = "mpg")
   )
 })
+
+test_that("add headline column returns columns", {
+  df <-
+    add_headline_column(
+      df = mtcars,
+      compare = gear,
+      reference = carb,
+      return_cols = dplyr::starts_with("delta")
+    )
+
+  expect_true("headline" %in% names(df))
+  expect_true(all(c("delta", "delta_p") %in% names(df)))
+})
