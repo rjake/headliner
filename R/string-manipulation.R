924b7d99d11394f49b01ba41ce7ac04e55b29f0e
Jake <rjake@sas.upenn.edu>
2022-05-24 02:46:51 -0400

    Fix add article logic (#88)
    * Expand logic for decimals, starts with 18, etc
    
    * Update roxygen
    
    * Update test-utils-data-manipulation.R
    
    * Remove vectorized example
    
    * Add logo to .Rbuildignore
    
    * Rerun docs
    
    * Remove unused line
    
    * Rerun vignette

--- R/string-manipulation.R
+++ R/string-manipulation.R
@@ -4,9 +4,35 @@
 #'
 #' @export
 #'
+#' @md
+#' @details
+#' This function uses crude logic to append 'a' or 'an' to numbers and phrases.
+#' * words that start with aeiou
+#' * negative numbers always start with 'a', ex: 'a -3' or 'a -8'
+#' * decimals always start with 'a' ex: 0.4 is usually pronounced
+#' 'a zero point four' or 'a point four'
+#' * numbers starting with 8 are always 'an'
+#' * if the integer that comes after thousand or million is 11 or 18 then 'an'
+#'   * 18,000 becomes 18 and that becomes 'an 18'
+#' * if the integer that comes after thousand or million is in
+#' 1, 10, 12, 13, 14, 15, 16, 17, 19 then 'a'
+#'   * 15,500 becomes 15 and that becomes 'a 15'
+#' * otherwise 'a'
+#'
 #' @examples
 #' add_article("increase")
+#'
 #' add_article("decrease")
+#'
+#' add_article(c(8, 11, 18))
+#'
+#' add_article(18123)
+#'
+#' stats::setNames(
+#'   add_article(1.8 * 10^(1:7)),
+#'   prettyNum(1.8 * 10^(1:7), big.mark = ",")
+#' )
+#'
 add_article <- function(x) {
   paste(get_article(x), x)
 }
