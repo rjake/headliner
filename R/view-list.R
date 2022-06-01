292d01043960bbf329f92d134a20114842711f7d
Jake <rjake@sas.upenn.edu>
2022-06-01 15:15:12 -0400

    Update view-list.R

--- R/view-list.R
+++ R/view-list.R
@@ -1,13 +1,16 @@
 #' Compact view of list values
-#'
-#' @param x list from 'compare_values()'
+#' @return returns a data frame to display a list or vector vertically
+#' @param x a vector or list to be transposed
 #' @export
 #' @seealso [compare_values()]
 #' @examples
 #' compare_values(10, 8) |>
 #'   view_list()
+#'
+#' add_article(c(1,8,10, 11, 18)) |>
+#'   view_list()
 view_list <- function(x) {
   data.frame(
-    VALUES = unlist(x)
+    value = unlist(x)
   )
 }
