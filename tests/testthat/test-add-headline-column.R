a54c5a55334eba0fea2810c16e717929630c7c46
Jake <rjake@sas.upenn.edu>
2022-06-26 00:22:11 -0400

    Update test-add-headline-column.R

--- tests/testthat/test-add-headline-column.R
+++ tests/testthat/test-add-headline-column.R
@@ -71,7 +71,11 @@ test_that("warning if columns renamed", {
     x = 1:3,
     y = 3:1
   ) |>
-  add_headline_column(y, x, return_cols = everything()) |>
+  add_headline_column(
+    x = y,
+    y = x,
+    return_cols = everything()
+  ) |>
   expect_message("New names")
 })
 
@@ -87,3 +91,26 @@ test_that("returns NA instead of error", {
   )
 })
 
+test_that("uses dynamic headline", {
+  headlines <- c(
+    "{other} has {trend}d",
+    "{delta_p}"
+  )
+
+  df <-
+    data.frame(
+      a = 1:2,
+      b = 2:3,
+      other = "car"
+    ) |>
+    mutate(phrase = headlines)
+
+  res <-
+    df |>
+    add_headline_column(a, b, phrase)
+
+  expect_equal(
+    c("car has decreased", "33.3"),
+    res$headline
+  )
+})
